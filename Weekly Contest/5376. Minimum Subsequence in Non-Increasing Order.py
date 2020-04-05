64fa2ecd7822be08d79427cbdb46a84220af44b7
jntushar <tjain4736@gmail.com>
2020-04-05 08:29:00 +0530

    Solution

---
+++ Weekly Contest/5376. Minimum Subsequence in Non-Increasing Order.py
@@ -0,0 +1,22 @@
+"""
+
+Given the array nums, obtain a subsequence of the array whose sum of elements is strictly greater than the sum of the non included elements in such subsequence.
+If there are multiple solutions, return the subsequence with minimum size and if there still exist multiple solutions, return the subsequence with the maximum total sum of all its elements. A subsequence of an array can be obtained by erasing some (possibly zero) elements from the array.
+Note that the solution with the given constraints is guaranteed to be unique. Also return the answer sorted in non-increasing order.
+
+"""
+
+
+"""---SOLUTION---"""
+
+
+class Solution:
+    def minSubsequence(self, nums):
+        res = []
+
+        for _ in range(len(nums)):
+            if sum(res)<=sum(nums):
+                res.append(max(nums))
+                nums.pop(nums.index(max(nums)))
+
+        return res
