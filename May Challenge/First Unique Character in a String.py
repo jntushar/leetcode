2fd37ac2b03bdd34122d4764e7cea329d058fa1b
jntushar <tjain4736@gmail.com>
2020-05-05 12:44:04 +0530

    Day 5

---
+++ May Challenge/First Unique Character in a String.py
@@ -0,0 +1,14 @@
+"""
+
+Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+
+"""
+
+"""---SOLUTION---"""
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        for i in s:
+            if s.count(i) == 1:
+                return s.index(i)
+        return -1
