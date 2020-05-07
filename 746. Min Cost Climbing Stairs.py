701e65a1a9b7cd2ab61e438396abd15a38b2d20b
jntushar <tjain4736@gmail.com>
2020-05-07 12:24:37 +0530

    Solution

---
+++ 746. Min Cost Climbing Stairs.py
@@ -0,0 +1,17 @@
+"""
+
+On a staircase, the i-th step has some non-negative cost cost[i] assigned (0 indexed).
+
+Once you pay the cost, you can either climb one or two steps. You need to find minimum cost to reach the top of the floor,
+and you can either start from the step with index 0, or the step with index 1.
+
+"""
+
+"""---SOLUTION---"""
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        f1 = f2 = 0
+        for x in reversed(cost):
+            f1, f2 = x + min(f1, f2), f1
+        return min(f1, f2)
