4f730590375a0bc9f04d221830bd7c6517f94fe7
Tushar Jain <tjain4736@gmail.com>
2020-05-08 17:30:20 +0530

    Update Check If It Is a Straight Line.py

--- May Challenge/Check If It Is a Straight Line.py
+++ May Challenge/Check If It Is a Straight Line.py
@@ -10,10 +10,8 @@ Check if these points make a straight line in the XY plane.
 
 class Solution:
     def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
-
         # Calculating slope of a line
         # m = (y2 - y1)/(x2 - x1)
-
         try:
             m = (coordinates[1][1] - coordinates[0][1]) / (coordinates[1][0] - coordinates[0][0])
         except:
@@ -21,11 +19,9 @@ class Solution:
 
         # Calculating Intercept
         # using y = mx+c
-
         c = coordinates[0][1] - m * coordinates[0][0]
 
         for i in range(len(coordinates)):
             if coordinates[i][1] != m * coordinates[i][0] + c:
                 return False
-
         return True
